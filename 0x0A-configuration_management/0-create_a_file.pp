4554c36626f9ef7fd6ab884b3d386664e6e3ed04
mlle-Kwamboka <ruthmayenga@gmail.com>
2022-09-16 07:18:16 -0700

    Add all

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#Creates a file in the /tmp directory
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
+
